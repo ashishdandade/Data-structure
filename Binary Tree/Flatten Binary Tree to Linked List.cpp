7a426ad812bd525ed5993ba405fcd60732d73158
ashishdandade <92213024+ashishdandade@users.noreply.github.com>
2021-10-17 20:31:16 +0530

    Create Flatten Binary Tree to Linked List.cpp

---
+++ Binary Tree/Flatten Binary Tree to Linked List.cpp
@@ -0,0 +1,16 @@
+//Morris Traversal
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+        while (curr) {
+            if (curr->left) {
+                TreeNode* runner = curr->left;
+                while (runner->right != nullptr) runner = runner->right;
+                runner->right = curr->right, curr->right = curr->left, curr->left = nullptr;
+            }
+            curr = curr->right;
+        }
+    }
+};
