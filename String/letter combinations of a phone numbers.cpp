e0a640e0a5d759e064250a44592cf7f6cd64a615
Ashish Dandade <92213024+ashishdandade@users.noreply.github.com>
2021-10-25 21:09:23 +0530

    Create letter combinations of a phone numbers.cpp

---
+++ String/letter combinations of a phone numbers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string>phone={"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if(digits.empty())return {};
+        queue<string>q;
+        vector<string>ans;
+        q.push("");
+        while(!q.empty()){
+            string s= q.front();
+            q.pop();
+            if(s.length()==digits.length()){
+                ans.push_back(s);
+            }
+            else{
+               
+                string temp = phone[digits[s.length()] - '0'];
+                for(auto i:temp){
+                    q.push(s+i);
+                }
+            }
+            
+        }
+        return ans;
+        
+    }
+};
