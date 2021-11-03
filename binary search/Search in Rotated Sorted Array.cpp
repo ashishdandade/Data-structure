a8e29b905aa4f70db8416f861aa1c10b431a74d2
Ashish Dandade <92213024+ashishdandade@users.noreply.github.com>
2021-11-03 21:31:49 +0530

    Update Search in Rotated Sorted Array.cpp

--- binary search/Search in Rotated Sorted Array.cpp
+++ binary search/Search in Rotated Sorted Array.cpp
@@ -1,17 +1,17 @@
-Core Observation - As we are are given a sorted and rotated array, it is guranteed that either 1/2 of the array is going to be sorted (If you aren't getting this try taking different combinations of provided example array)
-Now our first job is to find that sorted half. To do this we can do a simple comparison of mid (mid = (low+high)/2) element with the starting element (low) of the array.
-Comparison (do this with pen and paper in hand)
-if(nums[mid] >= nums[low]) -> the left half of array is sorted.
-else -> the right half of array is sorted.
-Now if left half of array is sorted:
-check if provided target element lies in the range [low, mid).
-If it does then update high = mid-1 (hence skipping the part present on right of mid).
-else update low = mid+1 (hence skipping the part present on left of mid)
-else as the right array is sorted:
-check if provided target element lies in the range (mid, high].
-If it does then update low = mid+1 (hence skipping the part present on left of mid).
-else update high = mid-1 (hence skipping the part present on right of mid)
-If still not found then return -1 (duh!)
+// Core Observation - As we are are given a sorted and rotated array, it is guranteed that either 1/2 of the array is going to be sorted (If you aren't getting this try taking different combinations of provided example array)
+// Now our first job is to find that sorted half. To do this we can do a simple comparison of mid (mid = (low+high)/2) element with the starting element (low) of the array.
+// Comparison (do this with pen and paper in hand)
+// if(nums[mid] >= nums[low]) -> the left half of array is sorted.
+// else -> the right half of array is sorted.
+// Now if left half of array is sorted:
+// check if provided target element lies in the range [low, mid).
+// If it does then update high = mid-1 (hence skipping the part present on right of mid).
+// else update low = mid+1 (hence skipping the part present on left of mid)
+// else as the right array is sorted:
+// check if provided target element lies in the range (mid, high].
+// If it does then update low = mid+1 (hence skipping the part present on left of mid).
+// else update high = mid-1 (hence skipping the part present on right of mid)
+// If still not found then return -1 (duh!)
 
 
 
