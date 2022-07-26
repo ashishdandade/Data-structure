0d91434795f3d31019fed00ecf3b6cf8b1a680a8
Ashish Dandade <92213024+ashishdandade@users.noreply.github.com>
2022-07-26 22:00:44 +0530

    Update add Binay.cpp

--- Bit Manipulation/add Binay.cpp
+++ Bit Manipulation/add Binay.cpp
@@ -1,3 +1,13 @@
+// Example 1:
+// Input: a = "11", b = "1"
+// Output: "100"
+    
+// Example 2:
+// Input: a = "1010", b = "1011"
+// Output: "10101"
+
+
+
 class Solution {
 public:
     string addBinary(string a, string b) {
