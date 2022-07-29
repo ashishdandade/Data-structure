15169491a4c985c3ed73c94f771cabb240cee421
Ashish Dandade <92213024+ashishdandade@users.noreply.github.com>
2022-07-29 22:18:51 +0530

    Create Can Make Arithmetic Progression From Sequence.cpp

---
+++ Array/Can Make Arithmetic Progression From Sequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int difference=arr[1]-arr[0];
+        if(arr.size()==2){
+            return true;
+        }
+        for(int i=2;i<arr.size();i++){
+            if(arr[i]-arr[i-1]!=difference){
+                return false;
+            }
+        }
+        return true;
+    }
+};
