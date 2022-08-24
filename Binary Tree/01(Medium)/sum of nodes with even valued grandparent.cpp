a04d491b92657b6cd078251fe768a282d9509c7f
Ashish Dandade <92213024+ashishdandade@users.noreply.github.com>
2022-08-24 09:51:36 +0530

    Create sum of nodes with even valued grandparent.cpp

---
+++ Binary Tree/01(Medium)/sum of nodes with even valued grandparent.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum=0;
+    void dfs(TreeNode* root){
+        if(!root) return;
+        if(root->val%2==0) {
+            if(root->left!=NULL){
+                if(root->left->left!=NULL) sum+=root->left->left->val;
+                if(root->left->right!=NULL) sum+=root->left->right->val;
+            }
+            if(root->right!=NULL){
+                if(root->right->left!=NULL) sum+=root->right->left->val;
+                if(root->right->right!=NULL) sum+=root->right->right->val;
+            }
+        }
+        dfs(root->left);
+        dfs(root->right);
+    }
+    int sumEvenGrandparent(TreeNode* root) {
+        dfs(root);
+        return sum;
+    }
+};
