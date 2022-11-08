5f7956c02dda42a8e51c90c010cee3f36d57376c
Ashish Dandade <92213024+ashishdandade@users.noreply.github.com>
2022-11-08 17:49:12 +0530

    Create Repeated DNA sequence.cpp

---
+++ sliding Window/Medium/Repeated DNA sequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<string,int>mp;
+        vector<string>ans;
+        if(s.size()<=10){
+            return ans;
+        }
+        for(int i=0;i<=s.size()-10;i++){
+            string str=s.substr(i,10);
+            mp[str]++;
+        }
+        for(auto x:mp){
+            if(x.second>1){
+                ans.push_back(x.first);
+            }
+        }
+        return ans;
+    }
+};
