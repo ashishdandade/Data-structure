211132839108f46ac5e9121cc6eff05bc52397ed
Ashish Dandade <92213024+ashishdandade@users.noreply.github.com>
2022-11-08 16:10:16 +0530

    Create BaseBallGame.cpp

---
+++ Stack/Easy/BaseBallGame.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        int val1,val2;
+        int ans=0;
+        stack<int>st;
+        for(auto i:ops){
+            if(i=="C"){
+                st.pop();
+            }
+            else if(i=="D"){
+                st.push(st.top()*2);
+            }
+            else if(i=="+"){
+                val1=st.top();
+                st.pop();
+                val2=st.top();
+                st.push(val1);
+                st.push(val1+val2);
+            }
+            else{
+                st.push(stoi(i));
+            }
+        }
+        while(st.size()!=0){
+            ans+=st.top();
+            st.pop();
+        }
+        return ans;
+        
+    }
+};
